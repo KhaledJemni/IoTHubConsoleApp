0f2b222d6921a19219baa573fe1e1d1e4a0fa08b
Khaled Jemni <khaled.jemni@hotmail.com>
2017-11-02 12:22:05 +0100

    -Application implemented -Nugets Added

--- ConsoleappIoT/ConsoleappIoT/Program.cs
+++ ConsoleappIoT/ConsoleappIoT/Program.cs
@@ -1,16 +1,56 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleappIoT
 {
     class Program
     {
+        private const string DeviceConnectionString = "HostName=TutorialHub.azure-devices.net;SharedAccessKeyName=iothubowner;DeviceId=MyDevice;SharedAccessKey=1tnE4rnywvZO5updbmN7J8mNcz65H7yOLxlcUYQqD2o=";
+        static ServiceClient serviceClient;
+
         static void Main(string[] args)
         {
+            serviceClient = ServiceClient.CreateFromConnectionString(DeviceConnectionString);
+            Program program = new Program();
+        }
 
+        public Program()
+        {
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+            SendEvent().Wait();
+
+            ReceiveCommands(deviceClient).Wait();
+        }
+
+        static async Task SendEvent()
+        {
+            string dataBuffer;
+            dataBuffer = "IoT in 90 Seconds";
+            Microsoft.Azure.Devices.Message eventMessage = new Microsoft.Azure.Devices.Message(Encoding.ASCII.GetBytes(dataBuffer));
+            await serviceClient.SendAsync("MyDevice", eventMessage);
+        }
+
+        async Task ReceiveCommands(DeviceClient deviceClient)
+        {
+            Console.WriteLine("\n Device waiting for IoT Hub command....\n");
+            Microsoft.Azure.Devices.Client.Message receivedMessage;
+            string messageData;
+            while (true)
+            {
+                receivedMessage = await deviceClient.ReceiveAsync(TimeSpan.FromSeconds(1));
+                if (receivedMessage != null)
+                {
+                    messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    Console.WriteLine("\t{0}> Message received: {1}", DateTime.Now.ToLocalTime(), messageData);
+                    await deviceClient.CompleteAsync(receivedMessage);
+                }
+            }
         }
     }
 }
